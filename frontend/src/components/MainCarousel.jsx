f747119950ebdbd35d18f452727ef9de59f2185c
JJFelix <felixomwenga441@gmail.com>
2024-06-09 22:20:27 +0300

    day's done

---
+++ frontend/src/components/MainCarousel.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./MainCarousel.css";
+
+import Carousel from "react-bootstrap/Carousel";
+import CarouselImage1 from "/images/sieuwert-otterloo-aren8nutd1Q-unsplash_6aa03d5ded604be58cd5ea00efdb0a6d.jpg";
+import CarouselImage2 from "/images/1_516b72b60bbb43428a3330cc03c287ff.jpg";
+import CarouselImage3 from "/images/todd-kent-178j8tJrNlc-unsplash_9fbc85da894043419dba3025c9818daf.jpg";
+import CarouselImage4 from "/images/ralph-ravi-kayden-2d4lAQAlbDA-unsplash_f685097ac49a426c8c1caa685b54c95c.jpg";
+import CarouselImage5 from "/images/etienne-beauregard-riverin-B0aCvAVSX8E-unsplash_d2ab9b9f78c542259668c00afb8b29fa.jpg";
+
+const images = [
+  CarouselImage1,
+  CarouselImage2,
+  CarouselImage3,
+  CarouselImage4,
+  CarouselImage5,
+];
+const MainCarousel = () => {
+  return (
+    <>
+      <div className="main-carousel">
+        {/* <Carousel className="carousel1">
+          {images.map((image, index) => (
+            <Carousel.Item key={index} className="carousel1-img">
+              <img src={image} alt="" />
+            </Carousel.Item>
+          ))}
+        </Carousel> */}
+      </div>
+    </>
+  );
+};
+
+export default MainCarousel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,17 @@ import './Home.css'
 
 import FeaturedProperties from '../components/FeaturedProperties.jsx'
 import FeaturedProperties1 from '../components/FeaturedProperties1.jsx'
+import MainCarousel from '../components/MainCarousel.jsx'
 
 
 const Home = () =>{
 
     return (
         <div className='main-wrapper'>
+            {/* Carousel */}
+            <MainCarousel />
+            <br />
+            <h2>Listed properties</h2>
             <FeaturedProperties />           
         </div>   
     )
